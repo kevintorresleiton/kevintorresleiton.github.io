531037ed47f71a596e5b87ad8edac5eea78f12ff
kevintorresleiton <49006012+kevintorresleiton@users.noreply.github.com>
2023-03-24 01:54:54 -0300

    initial commit

---
+++ function.js
@@ -0,0 +1,36 @@
+function mostrardatos() {
+    document.getElementById('datos').style.display = 'block';
+};
+function ocultardatos() {
+    document.getElementById('datos').style.display = 'none';
+};
+function mostraracademica() {
+    document.getElementById('academica').style.display = 'block';
+};
+function ocultaracademica() {
+    document.getElementById('academica').style.display = 'none';
+};
+function mostrarexperiencia() {
+    document.getElementById('experiencia').style.display = 'block';
+};
+function ocultarexperiencia() {
+    document.getElementById('experiencia').style.display = 'none';
+};
+function mostraridiomas() {
+    document.getElementById('idiomas').style.display = 'block';
+};
+function ocultaridiomas() {
+    document.getElementById('idiomas').style.display = 'none';
+};
+function mostrarpasatiempos() {
+    document.getElementById('pasatiempos').style.display = 'block';
+};
+function ocultarpasatiempos() {
+    document.getElementById('pasatiempos').style.display = 'none';
+};
+function mostrarimagen() {
+    document.getElementById('foto').style.display = 'block';
+};
+function ocultarimagen() {
+    document.getElementById('foto').style.display = 'none';
+};
